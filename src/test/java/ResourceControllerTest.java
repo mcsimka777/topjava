cd8050f4839bc5f2f9f2c5f89cbd2666864e804d
Maksim Volkov <m.s.volkov36@gmail.com>
2022-03-29 21:01:01 +0300

    HW07

---
+++ src/test/java/ResourceControllerTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ResourceControllerTest extends AbstractControllerTest {
+
+    private final String TEXT_CSS = "text/css;charset=UTF-8";
+
+    @Test
+    void requestResource() throws Exception {
+        perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(TEXT_CSS));
+    }
+}
